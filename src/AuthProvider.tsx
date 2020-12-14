ff20bad8d3bd9b3ea00b92947f8c19ca5c5f8c7b
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-14 16:51:26 -0600

    align types in context

--- src/AuthProvider.tsx
+++ src/AuthProvider.tsx
@@ -1,10 +1,23 @@
 import React, { FC, useState } from 'react';
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const AuthContext = React.createContext({});
+type User = null | { username: string };
+type TContext = {
+  user: User;
+  login: () => void;
+  logout: () => void;
+};
+
+const initialContext: TContext = {
+  user: null,
+  login: () => null,
+  logout: () => null,
+};
+
+export const AuthContext = React.createContext<TContext>(initialContext);
 
 export const AuthProvider: FC = ({ children }) => {
-  const [user, setUser] = useState<null | { [key: string]: string }>(null);
+  const [user, setUser] = useState<User>(null);
 
   return (
     <AuthContext.Provider
