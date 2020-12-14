6dbb5d43c8e1b53b1543c9b51623cd7ab640cc6e
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-14 17:18:20 -0600

    bottom tabs

--- src/AppTabs.tsx
+++ src/AppTabs.tsx
@@ -1,12 +1,31 @@
 import React, { FC } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AppParamList } from './AppParamList';
+import Center from './Center';
+import { Text } from 'react-native';
 
-const Tabs = createBottomTabNavigator();
+const Tabs = createBottomTabNavigator<AppParamList>();
+
+function Home() {
+  return (
+    <Center>
+      <Text>Home</Text>
+    </Center>
+  );
+}
+function Search() {
+  return (
+    <Center>
+      <Text>Search</Text>
+    </Center>
+  );
+}
 
 export const AppTabs: FC = ({}) => {
   return (
     <Tabs.Navigator>
-      <Tabs.Screen />
+      <Tabs.Screen name='Home' component={Home} />
+      <Tabs.Screen name='Search' component={Search} />
     </Tabs.Navigator>
   );
 };
